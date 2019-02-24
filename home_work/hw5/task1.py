6a67aa1dc69cf48190592fe7d818eb9436260abb
timoxa375 <47416652+timoxa375@users.noreply.github.com>
2019-02-24 20:34:04 +0300

    Delete task1.py

--- home_work/hw5/task1.py
+++
@@ -1,8 +0,0 @@
-matr = [[5,2,9,],[3,5,6,]]
-total_sum = 0
-num = 0
-for i in matr:
-    for j in i:
-        total_sum += j
-        num += 1
-print("Среднее арифметическое равно: ", int(total_sum / num))
