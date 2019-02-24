bd1ec799f290b1d4011e1cf7af8df94e678b8f7a
timoxa375 <47416652+timoxa375@users.noreply.github.com>
2019-02-24 20:33:21 +0300

    Delete task4.py

--- home_work/hw3/task4.py
+++
@@ -1,8 +0,0 @@
-mess = input("Please, enter your message:")
-list(mess)
-if "Mr./Mrs." in mess:
-    print("Gender is male")
-elif "Miss/Ms." in mess:
-    print("Gender is female")
-else:
-    print("No gender!")
